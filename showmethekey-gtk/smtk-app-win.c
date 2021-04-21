8223f8ec97fa74c9e7c7616dffde99bee7086b4b
Alynx Zhou <alynx.zhou@gmail.com>
2021-04-21 17:05:40 +0800

    Updated license display.

--- showmethekey-gtk/smtk-app-win.c
+++ showmethekey-gtk/smtk-app-win.c
@@ -293,14 +293,34 @@ void smtk_app_win_show_about_dialog(SmtkAppWin *win)
 
 	const gchar *documenters[] = { "Alynx Zhou", NULL };
 
-	gtk_show_about_dialog(
-		GTK_WINDOW(win), "authors", authors, "artists", artists,
-		"documenters", documenters, "translator-credits",
-		_("translator-credits"), "title", _("About Show Me The Key"),
-		"program-name", _("Show Me The Key"), "comments",
-		_("Show keys you typed on screen."), "copyright",
-		"Copyright © 2021 Alynx Zhou", "license-type",
-		GTK_LICENSE_APACHE_2_0, "logo-icon-name", "showmethekey",
-		"website", "https://showmethekey.alynx.one/", "website-label",
-		"showmethekey.alynx.one", "version", PROJECT_VERSION, NULL);
+	const gchar license[] =
+		"Licensed under the Apache License, "
+		"Version 2.0 (the \"License\");\n"
+		"you may not use this file except in "
+		"compliance with the License.\n"
+		"You may obtain a copy of the License at\n\n"
+
+		"    http://www.apache.org/licenses/LICENSE-2.0\n\n"
+
+		"Unless required by applicable law or "
+		"agreed to in writing, software\n"
+		"distributed under the License is distributed "
+		"on an \"AS IS\" BASIS,\n"
+		"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, "
+		"either express or implied.\n"
+		"See the License for the specific language "
+		"governing permissions and\n"
+		"limitations under the License.";
+
+	gtk_show_about_dialog(GTK_WINDOW(win), "authors", authors, "artists",
+			      artists, "documenters", documenters,
+			      "translator-credits", _("translator-credits"),
+			      "title", _("About Show Me The Key"),
+			      "program-name", _("Show Me The Key"), "comments",
+			      _("Show keys you typed on screen."), "copyright",
+			      "Copyright © 2021 Alynx Zhou", "license", license,
+			      "logo-icon-name", "showmethekey", "website",
+			      "https://showmethekey.alynx.one/",
+			      "website-label", "showmethekey.alynx.one",
+			      "version", PROJECT_VERSION, NULL);
 }
