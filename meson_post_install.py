bd246b390786664cd40dab9cfce63c40ae32fe9a
Alynx Zhou <alynx.zhou@gmail.com>
2022-06-28 17:46:48 +0800

    Updated to update icon cache on install.

--- meson_post_install.py
+++ meson_post_install.py
@@ -4,7 +4,16 @@ import os
 import sys
 import subprocess
 
+schema_dir = os.path.join(sys.argv[1], 'glib-2.0', 'schemas')
+icon_dir = os.path.join(sys.argv[1], 'icons', 'hicolor')
+
+# Package manager will set DESTDIR on packaging and run those on installing.
+# We do nothing if packaging.
 if not os.environ.get("DESTDIR"):
-    argv = ["glib-compile-schemas", sys.argv[1]]
+    argv = ["glib-compile-schemas", schema_dir]
+    print(" ".join(argv))
+    subprocess.call(argv)
+
+    argv = ["gtk-update-icon-cache", "-f", "-t", icon_dir]
     print(" ".join(argv))
     subprocess.call(argv)
